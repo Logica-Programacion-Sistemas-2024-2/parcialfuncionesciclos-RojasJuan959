1d95a4b37a6fb238467da94a55405c30de595064
RojasJuan959 <119331764+RojasJuan959@users.noreply.github.com>
2024-08-23 00:29:58 -0500

    Solucion

--- app/src/main/java/parcial1g2/App.java
+++ app/src/main/java/parcial1g2/App.java
@@ -19,9 +19,59 @@ public class App {
         * Controlar errores retornando -1
     */
     //------------------------------------------------------------------
+    public static int calcularTotalImpuestos(String decada, int avaluo) {
+        try {
+            int totalPago = 0;
+            double tasaRodamiento = 0;
+            double tasaSeguro = 0;
     
-
-
+            switch (decada) {
+                case "70":
+                    tasaRodamiento = 0.0376;
+                    tasaSeguro = 0.00876;
+                    break;
+                case "80":
+                    tasaRodamiento = 0.0398;
+                    tasaSeguro = 0.00854;
+                    break;
+                case "90":
+                    tasaRodamiento = 0.0409;
+                    tasaSeguro = 0.00816;
+                    break;
+                case "00":
+                    tasaRodamiento = 0.0434;
+                    tasaSeguro = 0.00798;
+                    break;
+                case "10":
+                    tasaRodamiento = 0.0493;
+                    tasaSeguro = 0.00712;
+                    break;
+                case "20":
+                    tasaRodamiento = 0.0568;
+                    tasaSeguro = 0.00699;
+                    break;
+                default:  // Para cualquier otra década no listada
+                    tasaRodamiento = 0.06;
+                    tasaSeguro = 0.009;
+                    break;
+            }
+    
+            // Calcular el impuesto de rodamiento
+            int impuestoRodamiento = (int) (avaluo * tasaRodamiento);
+    
+            // Calcular el seguro
+            int seguro = (int) (avaluo * tasaSeguro);
+    
+            // Calcular el total a pagar
+            totalPago = impuestoRodamiento + seguro;
+    
+            return totalPago;
+    
+        } catch (Exception e) {
+            // Si hay un error, devolver 0
+            return 0;
+        }
+    }
     //------------------------------------------------------------------
 
 }
